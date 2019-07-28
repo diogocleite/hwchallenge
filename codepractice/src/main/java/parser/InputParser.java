8c900853ffc064dc66c7198718714196cb9d247e
Diogo <dimcl5@gmail.com>
2019-07-28 20:12:13 +0100

    Add files via upload

---
+++ codepractice/src/main/java/parser/InputParser.java
@@ -0,0 +1,33 @@
+package parser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class InputParser {
+
+    private RandomNumbers randomNumbers = new RandomNumbers();
+
+    public static void main(String[] args)throws Exception
+    {
+        InputParser inputParser = new InputParser();
+        inputParser.readFile(args[0]);
+    }
+
+    //read a file
+    public void readFile (String pathFile) throws IOException {
+        File file = new File(pathFile);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String st;
+        while ((st = br.readLine()) != null)
+            randomNumbers.add(st);
+
+    }
+
+    public RandomNumbers getRandomNumbers() {
+        return randomNumbers;
+    }
+}
